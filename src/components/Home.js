6734177902404a4f1d8c0aa61abe4e8d9dc432e1
Mariana <marianaluizamr@gmail.com>
2020-11-27 15:31:23 +0000

    homepage css adjustments on mobile

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,3 +1,5 @@
+import { Link } from 'react-router-dom'
+
 import './Home.css'
 import Image from '../media/main-img-home.png'
 import Button from './smallComponents/Button'
@@ -14,19 +16,19 @@ export default function Home() {
                 <p className="general__code-caracters general__code-caracters--body">&#60;body&#62;</p>
 
                 <p className="general__code-caracters general__code-caracters--h1">&#60;h1&#62;</p>
-                <h1 className="home__title">Hey There, I’m <span>Mariana Reis</span></h1>
+                <h1 className="home__title">Hey There,<br></br> I’m Mariana Reis</h1>
                 <p className="general__code-caracters general__code-caracters--h1">&#60;/h1&#62;</p>
                 <p className="general__code-caracters general__code-caracters--h3">&#60;h3&#62;</p>
                 <p className="home__subtitle">Front-end Web Developer</p>
                 <p className="general__code-caracters general__code-caracters--h3">&#60;/h3&#62;</p>
-                <Button className="home__contact-button">CONTACT ME</Button>
+                <Link to="/contact"><Button className="home__contact-button">CONTACT ME</Button></Link>
 
                 <p className="general__code-caracters general__code-caracters--body">&#60;/body&#62;</p>
                 <p className="general__code-caracters general__code-caracters--html">&#60;/html&#62;</p>
                 <img src={Image} className="home__img" alt="MultiTask Girl" />
 
             </div>
-        </section>
+        </section >
 
     )
 }
--- src/components/smallComponents/Button.js
+++ src/components/smallComponents/Button.js
@@ -1,18 +1,22 @@
 import styled from 'styled-components'
-// import '../shared/_variables.scss'
 
 const Button = styled.button`
     background: transparent;
     border-radius: 5px;
-    border: 3px solid #49CBBF;
+    border: 3px solid #49CBBF; //$primaryColor2;
     color: #FFF;
+    font-size: 1rem;
     font-weight: 500;
     padding: 0.4rem 0.8rem;
     min-width: 150px;
     height: 40px;
 
     &:hover {
-        background: #49CBBF;
+        background: #49CBBF; //$primaryColor2;
+    }
+
+    &:active {
+        background: #49CBBF; //$primaryColor2;
     }
 `;
 
