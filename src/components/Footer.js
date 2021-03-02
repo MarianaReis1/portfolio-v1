8e7d239823c8f9c23e6c155a09e477d2b5ff8678
Mariana <marianaluizamr@gmail.com>
2021-03-02 12:36:25 +0000

    cv update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ import '../styles/Footer.css'
 export default function Footer() {
     return (
         <footer className="footer">
-           <p> Design and Developer by &copy; 2020 Mariana Reis</p>
+            <p> Designed and Developed by &copy; 2020 Mariana Reis</p>
         </footer>
     )
 }
